89eb452044de76835fbf48003108a56a01b2039c
DAXILSAXENA <daxildaxil@gmail.com>
2021-08-13 23:42:18 +0530

    [Daxil] Added:Ability to give Greeting Message with name

--- src/main/java/com/bridgelabz/greetingappdevelopment/controller/NewGreetingController.java
+++ src/main/java/com/bridgelabz/greetingappdevelopment/controller/NewGreetingController.java
@@ -1,5 +1,8 @@
 /**
- * Extend GreetingController to use Services Layer to get Simple Greeting message ”Hello World”
+ * Ability for the Greeting App to give Greeting message with
+ * 1. User First Name and Last Name
+ * 2. With just First Name or Last Name based on User attributes provided
+ * 3. Just Hello World.
  *
  * @author DAXIL SAXENA
  * @since 08.08.2021
@@ -12,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -27,4 +31,9 @@ public class NewGreetingController {
         return new ResponseEntity<>(greetingService.getMessage(), HttpStatus.OK);
     }
 
+    @GetMapping(value = "/getGreetingMessage")
+    public ResponseEntity<String> greeting(@RequestParam(value = "fname", defaultValue = "World") String fname,
+                                           @RequestParam(value = "lname", defaultValue = "") String lname) {
+        return new ResponseEntity<>(greetingService.getGreeting(fname, lname), HttpStatus.OK);
+    }
 }
--- src/main/java/com/bridgelabz/greetingappdevelopment/service/GreetingService.java
+++ src/main/java/com/bridgelabz/greetingappdevelopment/service/GreetingService.java
@@ -5,8 +5,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingService {
 
+    private static final String template = "Hello, %s %s!";
+
     public String getMessage() {
         return "Hello World";
+    }
+    public String getGreeting(String fname, String lname) {
+        return String.format(template, fname, lname);
     }
 
 }
